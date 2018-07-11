93fb13c0ad90a7abc6cb68ab869c671aa73a5667
Stanislav Grinkov <barlog.the.infernal@gmail.com>
2018-07-11 10:33:56 +0600

    Minor qol changes in env.sh

--- env.sh
+++ env.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 
-# this script requires tmux setting base-index and pane-base-index set to 1
+# this script requires tmux base-index and pane-base-index set to 1
 
-SESSION=python_dev_env
+SESSION=pydev
 
-tmux new-session -s "$SESSION" -n 'Python dev venv' -d
-tmux split-window -v -t "$SESSION" -p 20
-tmux send-keys -t "$SESSION:1.2" ' source venv/bin/activate && reset' C-m
-tmux send-keys -t "$SESSION:1.1" ' vim' C-m
+tmux has-session -t "$SESSION"
+if (( $? != 0 )); then
+    tmux new-session -s "$SESSION" -n 'Py dev' -d
+    tmux split-window -v -t "$SESSION" -p 20
+    tmux send-keys -t "$SESSION:1.2" ' source venv/bin/activate && reset' C-m
+    tmux send-keys -t "$SESSION:1.1" ' vim' C-m
 
-tmux new-window -t "$SESSION" -n 'GIT'
-tmux select-window -t "$SESSION:1"
-tmux select-pane -t "$SESSION:1.{top}"
-# tmux select-pane 
-tmux attach -t "$SESSION"
+    tmux new-window -t "$SESSION" -n 'GIT'
+    tmux select-window -t "$SESSION:1"
+    tmux select-pane -t "$SESSION:1.{top}"
+fi
+    tmux attach -t "$SESSION"
+
